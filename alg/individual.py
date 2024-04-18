d93356d08b90ad929478c5674fd77c7e9861f63d
kazukazupi <22kazuaki22@gmail.com>
2024-04-18 14:07:45 +0900

    added constructer

--- alg/individual.py
+++ alg/individual.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 from typing import Literal, NamedTuple, Optional, Tuple, Union
 
@@ -23,42 +24,39 @@ class Structure(NamedTuple):
         np.save(str(saving_dir / BODY_FILE_NAME), self.body)
         np.save(str(saving_dir / CONNECTIONS_FILE_NAME), self.connections)
 
+    @staticmethod
+    def load(saving_dir: Path):
+
+        # load structure
+        body = np.load(str(saving_dir / BODY_FILE_NAME))
+        connections = np.load(str(saving_dir / CONNECTIONS_FILE_NAME))
+
+        return Structure(body, connections)
+
 
 class IndividualInfo(BaseModel):
     id_: int
     generation: int
-    parents_id: Optional[Union[Tuple[int], Tuple[int, int]]]
-    fitness: Optional[float]
-    crossover_info: Optional[CrossoverInfo]
+    parents_id: Optional[Union[Tuple[int], Tuple[int, int]]] = None
+    fitness: Optional[float] = None
+    crossover_info: Optional[CrossoverInfo] = None
 
 
 class Individual:
 
     def __init__(
         self,
-        id_: int,
-        robot_shape: Tuple[int, int],
-        generation: int,
-        parents_id: Optional[Union[Tuple[int], Tuple[int, int]]] = None,
-        fitness: Optional[float] = None,
-        crossover_info: Optional[CrossoverInfo] = None,
+        structure: Structure,
+        info: IndividualInfo,
+        saving_dir: Path,
     ):
 
-        # initialize
-        self.structure = Structure(*sample_robot(robot_shape))
-        self.info = IndividualInfo(
-            id_=id_,
-            generation=generation,
-            parents_id=parents_id,
-            fitness=fitness,
-            crossover_info=crossover_info,
-        )
+        assert saving_dir.exists()
 
-        # make directory to save information
-        self.saving_dir = Path(
-            f"./experiment/generation{(self.generation):02}/id{(self.id_):02}"
-        )
-        self.saving_dir.mkdir(parents=False, exist_ok=False)
+        # initialize
+        self.structure = structure
+        self.info = info
+        self.saving_dir = saving_dir
 
     def save(self):
 
@@ -68,6 +66,34 @@ class Individual:
         # save robot info
         with open(self.saving_dir / JSON_FILE_NAME, "w") as fp:
             fp.write(self.info.model_dump_json(indent=3))
+
+    @classmethod
+    def init_random(
+        cls,
+        id_: int,
+        generation: int,
+        robot_shape: Tuple[int, int],
+    ):
+
+        structure = Structure(*sample_robot(robot_shape))
+        info = IndividualInfo(id_=id_, generation=generation)
+        saving_dir = Path(f"./experiment/generation{(generation):02}/id{(id_):02}")
+
+        saving_dir.mkdir(parents=False, exist_ok=False)
+
+        return cls(structure, info, saving_dir)
+
+    @classmethod
+    def load(cls, saving_dir: Path):
+
+        structure = Structure.load(saving_dir)
+
+        with open(saving_dir / JSON_FILE_NAME, "r") as fp:
+            json_data = json.load(fp)
+
+        info = IndividualInfo(**json_data)
+
+        return cls(structure, info, saving_dir)
 
     @property
     def id_(self):
