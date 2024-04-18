278164b3a5e5de0f540d556a68eea3c784a168bc
kazukazupi <22kazuaki22@gmail.com>
2024-04-18 11:46:55 +0900

    added Individual.save

--- alg/individual.py
+++ alg/individual.py
@@ -3,6 +3,9 @@ from typing import Literal, Optional, Tuple, TypedDict, Union
 
 import numpy as np
 
+BODY_FILE_NAME = "body.npy"
+CONNECTIONS_FILE_NAME = "connections.npy"
+
 
 class CrossoverInfo(TypedDict):
     axis: Literal[0, 1]
@@ -12,6 +15,15 @@ class CrossoverInfo(TypedDict):
 class Structure(TypedDict):
     body: np.ndarray
     connections: np.ndarray
+
+
+class IndividualInfo(TypedDict):
+    id_: int
+    generation: int
+    parents_id: Optional[Union[Tuple[int], Tuple[int, int]]]
+    learning_en: bool
+    fitness: Optional[float]
+    crossover_info: CrossoverInfo
 
 
 class Individual:
@@ -28,7 +40,7 @@ class Individual:
     ):
 
         # initialize
-        self.id = id_
+        self.id_ = id_
         self.generation = generation
         self.structure = structure
         self.parents_id = parents_id
@@ -36,7 +48,26 @@ class Individual:
         self.fitness = fitness
         self.crossover_info = crossover_info
 
+        # make directory to save information
         self.saving_dir = Path(
-            f"./experiment/generation{(self.generation + 1):02}/id{(self.id + 1):02}"
+            f"./experiment/generation{(self.generation + 1):02}/id{(self.id_ + 1):02}"
         )
         self.saving_dir.mkdir(parents=False, exist_ok=False)
+
+    def save(self):
+
+        # save structure
+        np.save(str(self.saving_dir / BODY_FILE_NAME), self.structure.body)
+        np.save(
+            str(self.saving_dir / CONNECTIONS_FILE_NAME), self.structure.connections
+        )
+
+        # save information in robot_info.json
+        individual_info = IndividualInfo(
+            id_=self.id_,
+            generation=self.generation,
+            parents_id=self.parents_id,
+            learning_en=self.learning_en,
+            fitness=self.fitness,
+            crossover_info=self.crossover_info,
+        )
